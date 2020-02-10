cead4f2bb0007fe5dd4b522054f4069c235871d6
caicai <shmily.cai@gmail.com>
2020-02-10 19:05:30 +0800

    前端知识点汇总

---
+++ javascript/27.async.js
@@ -0,0 +1,38 @@
+/***
+ *
+ * 模拟实现async/await函数中的async
+ *
+ * async函数的实现原理，就是将Generator函数和自动执行器，包装在一个函数里
+ */
+
+function spawn(genF) {
+  return new Promise(function(resolve, reject) {
+    const gen = genF();
+    function step(nextF) {
+      let next;
+      try {
+        next = nextF();
+      } catch (e) {
+        return reject(e);
+      }
+      if (next.done) {
+        return resolve(next.value);
+      }
+      Promise.resolve(next.value).then(
+        function(v) {
+          step(() => {
+            return gen.next(v);
+          });
+        },
+        function(e) {
+          step(() => {
+            return gen.throw(e);
+          });
+        }
+      );
+    }
+    step(() => {
+      return gen.next(undefined);
+    });
+  });
+}
