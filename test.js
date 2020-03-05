5bb201e6d5365c2a2dba04acbafad3f9b7ba62df
caicai <shmily.cai@gmail.com>
2020-03-05 09:19:14 +0800

    0304

--- test.js
+++
@@ -1,8 +0,0 @@
-function isType(type) {
-    return function (obj) {
-        return Object.prototype.toString.call(obj) === `[object ${type}] `
-    }
-}
-const isArray = isType('Array');
-const isString = isType('String');
-console.log(isArray([1, 2, [3, 4]]);
